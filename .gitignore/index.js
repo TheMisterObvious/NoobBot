a4f12f36c82ca6cbfcaed89f3de921e6c0bb67a9
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2019-02-12 00:38:33 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,7 +9,6 @@ var memberCount = client.users.size;
 var servercount = client.guilds.size;
     client.user.setActivity(prefix +'help')
     client.user.setAvatar('./avatar.jpg')
-    var servers = client.guilds.array().map(g => g.name).join(',');
     console.log("--------------------------------------");
 console.log('[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :)  \n[!]Les préfix actuelle:  ' + prefix + "\n[!]Mentions = " + mention + "\n[!]Nombre de membres: " + memberCount + "\n[!]Nombre de serveurs: " + servercount);
 });
