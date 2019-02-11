07481786fe27cd962627e264b442871fdd0b10cd
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2019-02-11 17:53:51 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+var prefix = "n!";
+var mention = "<@544560443776040980>";
+
+client.on("ready", () => {
+var memberCount = client.users.size;
+var servercount = client.guilds.size;
+    client.user.setGame('a!help')
+    client.user.setAvatar('./avatar.jpg')
+    var servers = client.guilds.array().map(g => g.name).join(',');
+    console.log("--------------------------------------");
+console.log('[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :)  \n[!]Les préfix actuelle:  ' + prefix + "\n[!]Mentions = " + mention + "\n[!]Nombre de membres: " + memberCount + "\n[!]Nombre de serveurs: " + servercount);
+});
+
+client.on("message", message => {
+  if 
+
+client.login(process.env.TOKEN);
